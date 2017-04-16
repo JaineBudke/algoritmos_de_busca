21c85a13036f5347466c35dede8234d8eaf83aa2
JaineBudke <jainebudke@hotmail.com>
2017-04-16 15:42:26 -0300

    Separando arquivos por chave de busca

--- src/main.cpp
+++ src/main.cpp
@@ -20,77 +20,240 @@ int main(){
 	int nBases = 5; // Quantidade de bases de busca
 	int tamN[] = { 50, 100, 1000, 10000, 100000 }; // Tamanhos das bases de busca
 
-	// GRAVANDO DADOS EM ARQUIVO dadosBI.txt
-	std::ofstream arqsaidaBI;
-	arqsaidaBI.open( "dadosBI.txt", std::ios::out );
-	if( !arqsaidaBI.is_open() ){
+	///////////////////////////////////////////////////////////////////////////
+	// GRAVANDO DADOS EM ARQUIVOS dadosBI1.txt, dadosBI2.txt, dadosBI3.txt
+	///////////////////////////////////////////////////////////////////////////
+	// Gravando em dadosBI1.txt
+	std::ofstream arqsaidaBI1;
+	arqsaidaBI1.open( "dadosBI1.txt", std::ios::out );
+	if( !arqsaidaBI1.is_open() ){
+		return 0;
+	}
+
+	std::cout << "Gerando dados BI..." << std::endl;
+	
+	// Grava o cabeçalho
+	arqsaidaBI1 << "#Dados para o script geraGrafico.gnu da Busca Binária Iterativa" << std::endl;
+	/*
+	 * X - Tamanho da base de busca
+	 * Y - Tempo de execução
+	 * W - Chave de busca						 
+	*/
+	arqsaidaBI1 << "#X" << std::setw(10) << "#Y" << std::endl;
+
+	// Gravando em dadosBI2.txt
+	std::ofstream arqsaidaBI2;
+	arqsaidaBI2.open( "dadosBI2.txt", std::ios::out );
+	if( !arqsaidaBI2.is_open() ){
 		return 0;
 	}
 
 	std::cout << "Gerando dados BI..." << std::endl;
 
 	// Grava o cabeçalho
-	arqsaidaBI << "#Dados para o script geraGrafico.gnu da Busca Binária Iterativa" << std::endl;
+	arqsaidaBI2 << "#Dados para o script geraGrafico.gnu da Busca Binária Iterativa" << std::endl;
 	/*
 	 * X - Tamanho da base de busca
 	 * Y - Tempo de execução
 	 * W - Chave de busca						 
 	*/
-	arqsaidaBI << "#X" << std::setw(10) << "#Y" << std::setw(10) << "#W" << std::endl;
+	arqsaidaBI2 << "#X" << std::setw(10) << "#Y" << std::endl;
 
-// GRAVANDO DADOS EM ARQUIVO dadosBR.txt
-	std::ofstream arqsaidaBR;
-	arqsaidaBR.open( "dadosBR.txt", std::ios::out );
-	if( !arqsaidaBR.is_open() ){
+	// Gravando em dadosBI3.txt
+	std::ofstream arqsaidaBI3;
+	arqsaidaBI3.open( "dadosBI3.txt", std::ios::out );
+	if( !arqsaidaBI3.is_open() ){
+		return 0;
+	}
+
+	std::cout << "Gerando dados BI..." << std::endl;
+
+	// Grava o cabeçalho
+	arqsaidaBI3 << "#Dados para o script geraGrafico.gnu da Busca Binária Iterativa" << std::endl;
+	/*
+	 * X - Tamanho da base de busca
+	 * Y - Tempo de execução
+	 * W - Chave de busca						 
+	*/
+	arqsaidaBI3 << "#X" << std::setw(10) << "#Y" << std::endl;
+
+
+	///////////////////////////////////////////////////////////////////////////
+	// GRAVANDO DADOS EM ARQUIVOS dadosBR1.txt, dadosBR2.txt, dadosBR3.txt
+	///////////////////////////////////////////////////////////////////////////
+	// Gravando dadosBR1.txt
+	std::ofstream arqsaidaBR1;
+	arqsaidaBR1.open( "dadosBR1.txt", std::ios::out );
+	if( !arqsaidaBR1.is_open() ){
 		return 0;
 	}
 
 	std::cout << "Gerando dados BR..." << std::endl;
 
 	// Grava o cabeçalho
-	arqsaidaBR << "#Dados para o script geraGrafico.gnu da Busca Binária Recursiva" << std::endl;
+	arqsaidaBR1 << "#Dados para o script geraGrafico.gnu da Busca Binária Recursiva" << std::endl;
 	/*
 	 * X - Tamanho da base de busca
 	 * Y - Tempo de execução
 	 * W - Chave de busca						 
 	*/
-	arqsaidaBR << "#X" << std::setw(10) << "#Y" << std::setw(10) << "#W" << std::endl;
+	arqsaidaBR1 << "#X" << std::setw(10) << "#Y" << std::endl;
 
-// GRAVANDO DADOS EM ARQUIVO dadosSI.txt
-	std::ofstream arqsaidaSI;
-	arqsaidaSI.open( "dadosSI.txt", std::ios::out );
-	if( !arqsaidaSI.is_open() ){
+	// Gravando dadosBR2.txt
+	std::ofstream arqsaidaBR2;
+	arqsaidaBR2.open( "dadosBR2.txt", std::ios::out );
+	if( !arqsaidaBR2.is_open() ){
+		return 0;
+	}
+
+	std::cout << "Gerando dados BR..." << std::endl;
+
+	// Grava o cabeçalho
+	arqsaidaBR2 << "#Dados para o script geraGrafico.gnu da Busca Binária Recursiva" << std::endl;
+	/*
+	 * X - Tamanho da base de busca
+	 * Y - Tempo de execução
+	 * W - Chave de busca						 
+	*/
+	arqsaidaBR2 << "#X" << std::setw(10) << "#Y" << std::endl;
+
+	// Gravando dadosBR3.txt
+	std::ofstream arqsaidaBR3;
+	arqsaidaBR3.open( "dadosBR3.txt", std::ios::out );
+	if( !arqsaidaBR3.is_open() ){
+		return 0;
+	}
+
+	std::cout << "Gerando dados BR..." << std::endl;
+
+	// Grava o cabeçalho
+	arqsaidaBR3 << "#Dados para o script geraGrafico.gnu da Busca Binária Recursiva" << std::endl;
+	/*
+	 * X - Tamanho da base de busca
+	 * Y - Tempo de execução
+	 * W - Chave de busca						 
+	*/
+	arqsaidaBR3 << "#X" << std::setw(10) << "#Y" << std::endl;
+
+
+	///////////////////////////////////////////////////////////////////////////
+	// GRAVANDO DADOS EM ARQUIVOS dadosSI1.txt, dadosSI2.txt, dadosSI3.txt
+	///////////////////////////////////////////////////////////////////////////
+	// Gravando dadosSI1.txt
+	std::ofstream arqsaidaSI1;
+	arqsaidaSI1.open( "dadosSI1.txt", std::ios::out );
+	if( !arqsaidaSI1.is_open() ){
 		return 0;
 	}
 
 	std::cout << "Gerando dados SI..." << std::endl;
 
 	// Grava o cabeçalho
-	arqsaidaSI << "#Dados para o script geraGrafico.gnu  da Busca Sequencial Iterativa" << std::endl;
+	arqsaidaSI1 << "#Dados para o script geraGrafico.gnu  da Busca Sequencial Iterativa" << std::endl;
 	/*
 	 * X - Tamanho da base de busca
 	 * Y - Tempo de execução
 	 * W - Chave de busca						 
 	*/
-	arqsaidaSI << "#X" << std::setw(10) << "#Y" << std::setw(10) << "#W" << std::endl;
+	arqsaidaSI1 << "#X" << std::setw(10) << "#Y" << std::endl;
 
-// GRAVANDO DADOS EM ARQUIVO dadosSR.txt
-	std::ofstream arqsaidaSR;
-	arqsaidaSR.open( "dadosSR.txt", std::ios::out );
-	if( !arqsaidaSR.is_open() ){
+	// Gravando dadosSI2.txt
+	std::ofstream arqsaidaSI2;
+	arqsaidaSI2.open( "dadosSI2.txt", std::ios::out );
+	if( !arqsaidaSI2.is_open() ){
+		return 0;
+	}
+
+	std::cout << "Gerando dados SI..." << std::endl;
+
+	// Grava o cabeçalho
+	arqsaidaSI2 << "#Dados para o script geraGrafico.gnu  da Busca Sequencial Iterativa" << std::endl;
+	/*
+	 * X - Tamanho da base de busca
+	 * Y - Tempo de execução
+	 * W - Chave de busca						 
+	*/
+	arqsaidaSI2 << "#X" << std::setw(10) << "#Y" << std::endl;
+
+	// Gravando dadosSI3.txt
+	std::ofstream arqsaidaSI3;
+	arqsaidaSI3.open( "dadosSI3.txt", std::ios::out );
+	if( !arqsaidaSI3.is_open() ){
+		return 0;
+	}
+
+	std::cout << "Gerando dados SI..." << std::endl;
+
+	// Grava o cabeçalho
+	arqsaidaSI3 << "#Dados para o script geraGrafico.gnu  da Busca Sequencial Iterativa" << std::endl;
+	/*
+	 * X - Tamanho da base de busca
+	 * Y - Tempo de execução
+	 * W - Chave de busca						 
+	*/
+	arqsaidaSI3 << "#X" << std::setw(10) << "#Y" << std::endl;
+
+
+
+	///////////////////////////////////////////////////////////////////////////
+	// GRAVANDO DADOS EM ARQUIVOS dadosSR1.txt, dadosSR2.txt, dadosSR3.txt
+	///////////////////////////////////////////////////////////////////////////
+	// Gravando dadosSR1.txt
+	std::ofstream arqsaidaSR1;
+	arqsaidaSR1.open( "dadosSR1.txt", std::ios::out );
+	if( !arqsaidaSR1.is_open() ){
 		return 0;
 	}
 
 	std::cout << "Gerando dados SR..." << std::endl;
 
 	// Grava o cabeçalho
-	arqsaidaSR << "#Dados para o script geraGrafico.gnu da Busca Sequencial Recursiva" << std::endl;
+	arqsaidaSR1 << "#Dados para o script geraGrafico.gnu da Busca Sequencial Recursiva" << std::endl;
 	/*
 	 * X - Tamanho da base de busca
 	 * Y - Tempo de execução
 	 * W - Chave de busca						 
 	*/
-	arqsaidaSR << "#X" << std::setw(10) << "#Y" << std::setw(10) << "#W" << std::endl;
+	arqsaidaSR1 << "#X" << std::setw(10) << "#Y" << std::endl;
+
+	// Gravando dadosSR2.txt
+	std::ofstream arqsaidaSR2;
+	arqsaidaSR2.open( "dadosSR2.txt", std::ios::out );
+	if( !arqsaidaSR2.is_open() ){
+		return 0;
+	}
+
+	std::cout << "Gerando dados SR..." << std::endl;
+
+	// Grava o cabeçalho
+	arqsaidaSR2 << "#Dados para o script geraGrafico.gnu da Busca Sequencial Recursiva" << std::endl;
+	/*
+	 * X - Tamanho da base de busca
+	 * Y - Tempo de execução
+	 * W - Chave de busca						 
+	*/
+	arqsaidaSR2 << "#X" << std::setw(10) << "#Y" << std::endl;
+
+	// Gravando dadosSR3.txt
+	std::ofstream arqsaidaSR3;
+	arqsaidaSR3.open( "dadosSR3.txt", std::ios::out );
+	if( !arqsaidaSR3.is_open() ){
+		return 0;
+	}
+
+	std::cout << "Gerando dados SR..." << std::endl;
+
+	// Grava o cabeçalho
+	arqsaidaSR3 << "#Dados para o script geraGrafico.gnu da Busca Sequencial Recursiva" << std::endl;
+	/*
+	 * X - Tamanho da base de busca
+	 * Y - Tempo de execução
+	 * W - Chave de busca						 
+	*/
+	arqsaidaSR3 << "#X" << std::setw(10) << "#Y" << std::endl;
+
+
+
 
 
 	for ( int i = 0 ; i < nBases ; ++i ) { // Fazendo várias buscas, com bases aleatórias de diferentes tamanhos
@@ -101,47 +264,56 @@ int main(){
 		generateRandomBase( base, tamBase ); // Gera base de busca aleatória e ordenada
 
 
-		int qntKeys = 3;
 		/* CHAVES DE BUSCA:
 		 * Melhor caso sequencial -> primeiro elemento = base[0].
 		 * Pior caso sequencial   -> elemento não existe = base[ tamBase-1 ]+1.
 		 * Melhor caso binária    -> elemento do meio = base[ tamBase/2 ].
 		 * Pior caso binária      -> elemento não existe = base[ tamBase-1 ]+1.
-		 */
-		int chavesDeBusca[] = { base[0], base[ tamBase-1 ]+1, base[ tamBase/2 ] };
-
+		*/
 
 		/* MEDIÇÃO DO TEMPO DE CADA IMPLEMENTAÇÃO DE BUSCA E COM CHAVES DE BUSCA DIFERENTES */
-		for ( int j = 0; j < qntKeys ; ++j ) {
-			tempoExecucaoBI( arqsaidaBI, base, tamBase, chavesDeBusca[j] );	
-		}
-		for ( int j = 0; j < qntKeys ; ++j ) {
-			tempoExecucaoBR( arqsaidaBR, base, tamBase, chavesDeBusca[j] );	
-		}
-		for ( int j = 0; j < qntKeys ; ++j ) {
-			tempoExecucaoSI( arqsaidaSI, base, tamBase, chavesDeBusca[j] );	
-		}
-		for ( int j = 0; j < qntKeys ; ++j ) {
-			tempoExecucaoSR( arqsaidaSR, base, tamBase, chavesDeBusca[j] );	
-		}
+		tempoExecucaoBI( arqsaidaBI1, base, tamBase, base[0] );	
+		tempoExecucaoBI( arqsaidaBI2, base, tamBase, base[ tamBase-1 ]+1 );	
+		tempoExecucaoBI( arqsaidaBI3, base, tamBase, base[ tamBase/2 ] );	
+
+		tempoExecucaoBR( arqsaidaBR1, base, tamBase, base[0] );	
+		tempoExecucaoBR( arqsaidaBR2, base, tamBase, base[ tamBase-1 ]+1 );	
+		tempoExecucaoBR( arqsaidaBR3, base, tamBase, base[ tamBase/2 ] );	
+
+		tempoExecucaoSI( arqsaidaSI1, base, tamBase, base[0] );	
+		tempoExecucaoSI( arqsaidaSI2, base, tamBase, base[ tamBase-1 ]+1 );	
+		tempoExecucaoSI( arqsaidaSI3, base, tamBase, base[ tamBase/2 ] );	
+
+		tempoExecucaoSR( arqsaidaSR1, base, tamBase, base[0] );	
+		tempoExecucaoSR( arqsaidaSR2, base, tamBase, base[ tamBase-1 ]+1 );	
+		tempoExecucaoSR( arqsaidaSR3, base, tamBase, base[ tamBase/2 ] );	
 
 	}
 
 	// FECHANDO ARQUIVO COM DADOS BI
 	std::cout << "Fechando o arquivo dadosBI.txt..." << std::endl;
-	arqsaidaBI.close();
+	arqsaidaBI1.close();
+	arqsaidaBI2.close();
+	arqsaidaBI3.close();
 
 	// FECHANDO ARQUIVO COM DADOS BR
 	std::cout << "Fechando o arquivo dadosBR.txt..." << std::endl;
-	arqsaidaBR.close();
+	arqsaidaBR1.close();
+	arqsaidaBR2.close();
+	arqsaidaBR3.close();
 
 	// FECHANDO ARQUIVO COM DADOS SI
 	std::cout << "Fechando o arquivo dadosSI.txt..." << std::endl;
-	arqsaidaSI.close();
+	arqsaidaSI1.close();
+	arqsaidaSI2.close();
+	arqsaidaSI3.close();
 
 	// FECHANDO ARQUIVO COM DADOS SR
 	std::cout << "Fechando o arquivo dadosSR.txt..." << std::endl;
-	arqsaidaSR.close();
+	arqsaidaSR1.close();
+	arqsaidaSR2.close();
+	arqsaidaSR3.close();
+
 	
 	return 0;
 
@@ -164,10 +336,10 @@ int tempoExecucaoBI( std::ofstream & arqsaidaBI, int *V, int n, int x ){
     Ticks[0] = clock();
 	bBinariaIterativa( V, n, x );
 	Ticks[1] = clock();
-	double tempoBI = (Ticks[1] - Ticks[0]) * 1000.0 / CLOCKS_PER_SEC;
+	double tempo = (Ticks[1] - Ticks[0]) * 1000.0 / CLOCKS_PER_SEC;
 
 	// Gravando os registros numéricos
-	arqsaidaBI << n << std::setw(10) << tempoBI << std::setw(10) << std::setw(10) << x << std::endl;
+	arqsaidaBI << n << std::setw(10) << tempo << x << std::endl;
 
 	// Tratamento de erro de leitura de arquivo
 	if( arqsaidaBI.fail() ) {
@@ -199,7 +371,7 @@ int tempoExecucaoBR( std::ofstream & arqsaidaBR, int *V, int n, int x ){
 	double tempoBR = (Ticks[3] - Ticks[2]) * 1000.0 / CLOCKS_PER_SEC;
 
 	// Gravando os registros numéricos
-	arqsaidaBR << n << std::setw(10) << tempoBR << std::setw(10) << std::setw(10) << x << std::endl;
+	arqsaidaBR << n << std::setw(10) << tempoBR << std::endl;
 
 	// Tratamento de erro de leitura de arquivo
 	if( arqsaidaBR.fail() ) {
@@ -231,7 +403,7 @@ int tempoExecucaoSI( std::ofstream & arqsaidaSI, int *V, int n, int x ){
 	double tempoSI = (Ticks[5] - Ticks[4]) * 1000.0 / CLOCKS_PER_SEC;
 
 	// Gravando os registros numéricos
-	arqsaidaSI << n << std::setw(10) << tempoSI << std::setw(10) << std::setw(10) << x << std::endl;
+	arqsaidaSI << n << std::setw(10) << tempoSI << std::endl;
 
 	// Tratamento de erro de leitura de arquivo
 	if( arqsaidaSI.fail() ) {
@@ -262,7 +434,7 @@ int tempoExecucaoSR( std::ofstream & arqsaidaSR, int *V, int n, int x ){
 	double tempoSR = (Ticks[7] - Ticks[6]) * 1000.0 / CLOCKS_PER_SEC;
 
 	// Gravando os registros numéricos
-	arqsaidaSR << n << std::setw(10) << tempoSR << std::setw(10) << std::setw(10) << x << std::endl;
+	arqsaidaSR << n << std::setw(10) << tempoSR << std::endl;
 
 	// Tratamento de erro de leitura de arquivo
 	if( arqsaidaSR.fail() ) {
